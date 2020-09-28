8c704140d25c37a3727539e6b0f253a7d88f2c34
RossCr0ss <sobechko.r@gmail.com>
2020-09-28 14:41:20 +0300

    add components

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './header.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Hello Star Wars!</h1>
+            </div>
+        )
+    }
+}
